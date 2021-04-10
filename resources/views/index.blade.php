5e075a7441241b1e005a69aa2c1f00afda6f4d2f
diegopa <diego.loge@hotmail.com>
2021-04-10 16:25:27 -0500

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <!--header-->
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <span class="fs-4">Simple header</span>
+            </a>
+
+            <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active">Inicio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Información</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Estadísticas</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Acerca de Nosotros</a></li>
+            </ul>
+        </header>
+    </div>
+    
+    <div class="container">
+        <p>La confusión de la población respecto a los síntomas del covid 19, provoca que llamen ambulancias o acudan a hospitales muchas veces incluso cuando no tienen la enfermedad entorpeciendo el sistema de salud y su pronta respuesta. Por ello este sistema permite el autodiagnóstico sobre la enfermedad de manera intuitiva para la población en general y permite conocer la posibilidad de infección por covid 19, evitando la sobresaturación de hospitales y ambulancias.</p>
+        <img src="" class="rounded mx-auto d-block" alt="...">
+    </div>
+
+</body>
+</html>
